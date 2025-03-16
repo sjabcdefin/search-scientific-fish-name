44bff130805a5207feff34e0b8eee7ce686d903c
tomoka <tomoka.saiwaki@gmail.com>
2025-03-16 16:26:34 +0900

    Add fish name and serch method processing
    魚の名前の入力と検索方法の選択処理を追加

---
+++ fish_name_app.js
@@ -0,0 +1,90 @@
+import enquirer from "enquirer";
+
+class FishNameApp {
+  static #searchMethods = {
+    EXACT: "完全一致",
+    PREFIX: "前方一致",
+    SUFFIX: "後方一致",
+    PARTIAL: "部分一致",
+  };
+
+  async run() {
+    try {
+      const fishName = await this.#askFishName();
+      const searchMethod = await this.#askSearchMethod();
+
+      switch (searchMethod) {
+        case FishNameApp.#searchMethods.EXACT:
+          console.log(
+            `fishName: ${fishName}, searchMethod: ${FishNameApp.#searchMethods.EXACT}`
+          );
+          break;
+        case FishNameApp.#searchMethods.PREFIX:
+          console.log(
+            `fishName: ${fishName}, searchMethod: ${FishNameApp.#searchMethods.PREFIX}`
+          );
+          break;
+        case FishNameApp.#searchMethods.SUFFIX:
+          console.log(
+            `fishName: ${fishName}, searchMethod: ${FishNameApp.#searchMethods.SUFFIX}`
+          );
+          break;
+        case FishNameApp.#searchMethods.PARTIAL:
+          console.log(
+            `fishName: ${fishName}, searchMethod: ${FishNameApp.#searchMethods.PARTIAL}`
+          );
+          break;
+      }
+    } catch (err) {
+      console.error(`エラー：${err.message}`);
+    }
+  }
+
+  async #cleanupAndExit() {
+    console.log("\nCtrl+C was detected during operation.");
+    process.exit(1);
+  }
+
+  async #askFishName() {
+    try {
+      const question = [
+        {
+          type: "input",
+          name: "fishName",
+          message: "検索したい魚の名前をカタカナで入力してください。：",
+        },
+      ];
+      const response = await enquirer.prompt(question);
+      return response.fishName ? response.fishName : undefined;
+    } catch (err) {
+      if (err === "") {
+        await this.#cleanupAndExit();
+      } else {
+        throw err;
+      }
+    }
+  }
+
+  async #askSearchMethod() {
+    try {
+      const question = [
+        {
+          type: "select",
+          name: "searchMethod",
+          message: "検索方法を選択してください。：",
+          choices: Object.values(FishNameApp.#searchMethods),
+        },
+      ];
+      const answer = await enquirer.prompt(question);
+      return answer.searchMethod;
+    } catch (err) {
+      if (err === "") {
+        await this.#cleanupAndExit();
+      } else {
+        throw err;
+      }
+    }
+  }
+}
+
+export default FishNameApp;
